9c8e5f7cae42357ef6bcd7e0d36210c7cbf94cc6
jair <jairfloresaa0802@gmail.com>
2026-01-08 21:29:40 +0100

    actualizando  archivo custom.d.ts

--- code/custom.d.ts
+++ code/custom.d.ts
@@ -1,9 +1,19 @@
 import mongoose from "mongoose";
 import { DefaultSession } from "next-auth";
 
+import mongoose from "mongoose";
+
 declare global {
-    var mongoose: mongoose;
+  // eslint-disable-next-line no-var
+  var mongoose:
+    | {
+        conn: typeof mongoose | null;
+        promise: Promise<typeof mongoose> | null;
+      }
+    | undefined;
 }
+
+export {};
 
 declare module "next-auth" {
     interface Session {
